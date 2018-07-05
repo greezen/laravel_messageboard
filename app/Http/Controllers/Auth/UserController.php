b45a43b376afe64a8b8b97515436581402f8b817
greezen <852820276@qq.com>
2018-07-05 14:29:53 +0800

    support api

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -35,7 +35,7 @@ class UserController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required',
-            'email' => 'required|email',
+            'email' => 'required|email|unique:users,email',
             'password' => 'required',
             'c_password' => 'required|same:password',
         ]);
